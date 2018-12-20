9ef46671857f43c01b2f40f67b15ab1d4045ce7b
Maharoz <mahruj66@gmail.com>
2018-12-21 00:30:01 +0600

    binding to custom event done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,5 +8,20 @@ export class AppComponent {
   serverElements = [{type : 'server' , name: 'TestServer', content : 'Just a server'}];
 
 
+  onAddServerAdded(serverData:{serverName : string, serverContent: string}) {
+    this.serverElements.push({
+      type: 'server',
+      name: serverData.serverName,
+      content: serverData.serverContent
+    });
+  }
+
+  onAddBlueprintAdded(blueprintData:{serverName : string, serverContent: string}) {
+    this.serverElements.push({
+      type: 'blueprint',
+      name: blueprintData.serverName,
+      content: blueprintData.serverContent
+    });
+  }
  
 }
