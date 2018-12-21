99f85a40fc6fe137edefeb6681078c27c01f7166
Maharoz <mahruj66@gmail.com>
2018-12-21 08:50:25 +0600

    use of local reference demonstrated

--- src/app/cockpit/cockpit.component.ts
+++ src/app/cockpit/cockpit.component.ts
@@ -8,7 +8,7 @@ import { Output, EventEmitter } from '@angular/core';
 export class CockpitComponent implements OnInit {
   @Output() serverCreated = new EventEmitter<{serverName : string, serverContent: string }>();
   @Output('bpCreated') blueprintCreated = new EventEmitter<{serverName : string, serverContent: string }>();;
-  newServerName = '';
+ // newServerName = '';
   newServerContent = '';
 
   constructor() { }
@@ -17,14 +17,17 @@ export class CockpitComponent implements OnInit {
   }
 
 
-  onAddServer() {
-  this.serverCreated.emit({serverName : this.newServerName 
-    , serverContent : this.newServerContent});
+  onAddServer(nameInput :HTMLInputElement) {
+    console.log(nameInput.value)
+  this.serverCreated.emit({
+    serverName : nameInput.value, 
+     serverContent : this.newServerContent});
   }
 
-  onAddBlueprint() {
-    this.blueprintCreated.emit({serverName : this.newServerName 
-      , serverContent : this.newServerContent});
+  onAddBlueprint(nameInput :HTMLInputElement) {
+    this.blueprintCreated.emit({
+      serverName :nameInput.value, 
+     serverContent : this.newServerContent});
   }
 
 }
